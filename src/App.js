e47680714a6598785b4a26980198c8589079aacf
LemPetro <alexandru.dunose@gmail.com>
2021-03-24 21:56:17 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import React from "react";
+
+function App() {
+  return (
+    <header>
+      <nav className="nav-main">
+        <div className="nav-bar">
+          <h1 className="title-box">Lem Petro</h1>
+          <div className='menu'>
+            <h2>Home</h2>
+            <h2>Projects</h2>
+            <h2>About</h2>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default App;
